a9c97bb515b799fb267848619f41248f84fd993f
sharuksk <sharukajmal2@gmail.com>
2025-12-13 20:54:05 +0300

    deployment configs

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,27 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
+import path from 'path';
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.get('/health', (req, res) => {
   res.status(200).json({"message": 'API is running!'});
 });
 
+app.get('/books', (req, res) => {
+  res.status(200).json({"message": 'this is books endpoint'});
+});
 
+// mske ready for deployment
+if (ENV.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('/{*any}', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
 
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on port ${ENV.PORT}`);
