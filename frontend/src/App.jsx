45962e85bf3d2456c4dd422bde3de50a3fba3fca
sharuksk <sharukajmal2@gmail.com>
2026-01-03 14:19:25 +0300

    problem details route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,6 +27,7 @@ function App() {
         <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
 
         <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+        <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
       </Routes>
 
       <Toaster toastOptions={{ duration: 3000 }} />
